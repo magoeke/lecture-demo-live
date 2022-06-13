ba5c790f039bdf0ff404047d65db2bc9f19d408c
magoeke <maximilian.goeke@doubleslash.de>
2022-06-13 10:29:29 +0200

    Renamed test name.

--- src/test/java/de/magoeke/lecture/demo/bowling/control/BowlingGameTest.java
+++ src/test/java/de/magoeke/lecture/demo/bowling/control/BowlingGameTest.java
@@ -5,12 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class BowlingGameTest {
 
     @Test
-    void shouldReturn0WhenAllRollsWereZero() {
+    void should_return_0_when_all_rolls_were_zero() {
         final var bowlingGame = new BowlingGame();
 
         var rolls = new ArrayList<Integer>();
